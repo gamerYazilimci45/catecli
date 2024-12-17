d7a5dea99f6cf491e77b7de2203db172c1b8e07e
gamerYazilimci67 <abdulbakisari54@gmail.com>
2024-12-17 22:36:33 +0300

    hatalar düzelti, belirli dosyalar güncellendi

--- main.cpp
+++ main.cpp
@@ -7,43 +7,41 @@ namespace fs = std::filesystem;
 
 int main(int argc, char* argv[]) {
 
-    if (argv[1] == "-n"){
+    if (std::string(argv[1]) == "-n") {
         std::string folderName = argv[2];
-    
-    
+
         try {
-            std::string homePath = std::getenv("HOME");
+            const char* homeCStr = std::getenv("HOME");
+            if (!homeCStr) {
+                std::cerr << "Error: HOME environment variable is not set.\n";
+                return 1;
+            }
+            std::string homePath = homeCStr;
             fs::path configPath = homePath + "/.config/catecli";
+
             if (!fs::exists(configPath)) {
                 fs::create_directories(configPath);
-
             }
-            if(!fs::exists(folderName)) {
+
+            fs::path folderPath = fs::absolute(folderName).string();
+
+            if (!fs::exists(folderName)) {
                 fs::create_directory(folderName);
                 std::cout << "New Category Folder is Created: " << folderName << "\n";
-        
+            } else {
+                std::cout << "Category already exists!" << "\n";
             }
-            else{
-                std::cout << "Category is already exists!" << "\n";
-            }
-            std::string folderPath = fs::absolute(folderName).string();
+
             std::ofstream outFile(configPath / "categories.txt", std::ios::app);
             if (outFile.is_open()) {
                 outFile << folderPath << "\n";
-                outFile.close();
             } else {
+                std::cerr << "Error: Unable to open categories.txt for writing.\n";
                 return 1;
             }
-        }
-
-        
-        catch (const std::exception& e) {
+        } catch (const std::exception& e) {
             std::cerr << "Error: " << e.what() << "\n";
             return 1;
         }
-    }
     return 0;
-
-
-    
 }
